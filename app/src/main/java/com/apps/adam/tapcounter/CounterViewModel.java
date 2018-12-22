8e3bf7b6639095653a6d349b6cc344903f930d49
Adam <datriphopper@gmail.com>
2018-12-21 19:11:39 -0600

    Added comments

--- app/src/main/java/com/apps/adam/tapcounter/CounterViewModel.java
+++ app/src/main/java/com/apps/adam/tapcounter/CounterViewModel.java
@@ -4,5 +4,6 @@ import androidx.lifecycle.ViewModel;
 
 public class CounterViewModel extends ViewModel
 {
+    //Member variable to hold current value of counter
     public int currentCounter = 0;
 }
--- app/src/main/java/com/apps/adam/tapcounter/MainActivity.java
+++ app/src/main/java/com/apps/adam/tapcounter/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.lifecycle.ViewModelProviders;
 
 public class MainActivity extends AppCompatActivity
 {
+    //Instance of our ViewModel subclass
     CounterViewModel mViewModel;
 
     @Override
@@ -16,22 +17,26 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Connects ViewModel subclass to the Activity
         mViewModel = ViewModelProviders.of(this).get(CounterViewModel.class);
         displayCounter(mViewModel.currentCounter);
     }
 
+    //Increments counter by 1, used in onClick property of btn1
     public void incrementCounter(View v)
     {
         mViewModel.currentCounter = mViewModel.currentCounter + 1;
         displayCounter(mViewModel.currentCounter);
     }
 
+    //Displays current counter value
     public void displayCounter(int count)
     {
         TextView counterView = (TextView) findViewById(R.id.display);
         counterView.setText(String.valueOf(count));
     }
 
+    //Sets counter back to 0, used in onClick property of btn2
     public void resetCounter(View v)
     {
         mViewModel.currentCounter = 0;
